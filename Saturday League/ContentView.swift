312c4126bd682967616b6f498dce8098dcf4512a
sachin-gurung <saching061@hotmail.com>
2024-06-17 22:59:59 -0700

    Initial Commit

---
+++ Saturday League/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Saturday League
+//
+//  Created by Sachin Gurung on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Saturday League/Saturday_LeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saturday_LeagueApp.swift
+//  Saturday League
+//
+//  Created by Sachin Gurung on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Saturday_LeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
