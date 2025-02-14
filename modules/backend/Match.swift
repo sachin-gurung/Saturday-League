622e2d70f3f941f5f1cf33b419680ba97546acb3
sachin-gurung <saching061@hotmail.com>
2025-02-14 15:20:59 -0800

    Matches tab works and it stores the matches history. Standings tab works and updates table accordingly.

--- modules/backend/Match.swift
+++ modules/backend/Match.swift
@@ -1,11 +1,5 @@
-//
-//  Match.swift
-//  Saturday League
-//
-//  Created by Sachin Gurung on 6/17/24.
-//
-
 import Foundation
+import FirebaseFirestore
 
 struct Match: Identifiable {
     var id: String
@@ -13,32 +7,20 @@ struct Match: Identifiable {
     var homeScore: Int
     var awayTeam: String
     var awayScore: Int
-    var datePlayed: String
+    var datePlayed: Date
 
-    // Custom initializer to handle Firestore data parsing
     init?(id: String, data: [String: Any]) {
         guard let homeTeam = data["homeTeam"] as? String,
               let homeScore = data["homeScore"] as? Int,
               let awayTeam = data["awayTeam"] as? String,
               let awayScore = data["awayScore"] as? Int,
-              let datePlayed = data["datePlayed"] as? String else {
-            return nil // Return nil if any field is missing
-        }
-        
+              let datePlayedTimestamp = data["datePlayed"] as? Timestamp else { return nil }
+
         self.id = id
         self.homeTeam = homeTeam
         self.homeScore = homeScore
         self.awayTeam = awayTeam
         self.awayScore = awayScore
-        self.datePlayed = datePlayed
+        self.datePlayed = datePlayedTimestamp.dateValue()
     }
 }
-
-//import Foundation
-//
-//struct Match {
-//    var homeTeam: String
-//    var homeScore: Int
-//    var awayTeam: String
-//    var awayScore: Int
-//}
